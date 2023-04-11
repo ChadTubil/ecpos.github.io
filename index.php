d1f36f56a9e7dd8c06080d7d9522f4c68f16a9cd
Charles Tubil <charlestubil@gmail.com>
2023-04-11 22:22:10 +0800

    Initial Commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+    <head>
+
+        <title>EC POS</title>
+
+        <link rel="stylesheet" href="css/style.css"/>
+
+        <meta charset="utf-8">
+        <!-- Mobile Specific Metas
+        ================================================== -->
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <meta name="theme-color" content="#212121"/>
+        <meta name="msapplication-navbutton-color" content="#212121"/>
+        <meta name="apple-mobile-web-app-status-bar-style" content="#212121"/>
+
+        <script src="https://kit.fontawesome.com/78e605f6ef.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <div class="topbar">
+            <div class="topbarrow">
+                <div class="topbarcol">
+                    <h5 class="topbartext" id="topbar_content"><i class="fa-regular fa-clock"></i> 10:00AM To 10:00PM &nbsp&nbsp&nbsp&nbsp<i class="fa-sharp fa-solid fa-location-dot"></i> Candaba, Pampanga 2013</h5>
+                </div>
+                <div class="topbarcol"> 
+                    <h5 class="topbartext" style="text-align: right" id="topbar_content"><i class="fa-solid fa-phone"></i> 091234567890 &nbsp&nbsp&nbsp&nbsp<i class="fa-solid fa-envelope"></i> info@ecpos.com</h5>
+                </div>
+            </div>
+        </div>
+        <div class="aa">
+            asdasd
+        </div>
+    </body>
+</html>
